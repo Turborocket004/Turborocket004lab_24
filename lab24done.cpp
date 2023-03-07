1010b73a016d1aee1aecd399d3c2615b1c424a82
Turborocket004 <120168125+Turborocket004@users.noreply.github.com>
2023-03-07 16:29:31 +0700

    Update and rename lab24.cpp to lab24done.cpp

--- lab24done.cpp
+++ lab24done.cpp
@@ -24,11 +24,77 @@ ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
 	return ComplexNumber(real+c.real,imag+c.imag);
 }
 
+ComplexNumber operator+(double s,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
 ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
-//Write your code here
+ComplexNumber operator-(double s,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(real*c.real-imag*c.imag,real*c.imag+imag*c.real);
+}
+
+ComplexNumber operator*(double s,const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	double denom = c.real*c.real + c.imag*c.imag;
+	return ComplexNumber((real*c.real + imag*c.imag)/denom,(imag*c.real - real*c.imag)/denom);
+}
+
+ComplexNumber operator/(double s,const ComplexNumber &c){
+	double denom = c.real*c.real + c.imag*c.imag;
+	return ComplexNumber((s*c.real)/denom,(- s*c.imag)/denom);
+}
+
+ostream & operator<<(ostream &os,const ComplexNumber &c){
+	if (c.real == 0 && c.imag == 0)
+	{
+		return os << "0" ;
+	}
+	else if (c.real == 0)
+	{
+		return os << c.imag << "i";
+	}
+	else if (c.imag == 0)
+	{
+		return os << c.real;
+	}
+	else if (c.imag <= 0){
+		return os << c.real << c.imag << "i";
+	}
+	else{
+		return os << c.real << "+" << c.imag << "i" ;
+	}
+	
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	return (real == c.real && imag == c.imag);
+}
+
+bool operator==(double s,const ComplexNumber &c){
+	return (s == c.real && 0 == c.imag);
+}
+
+double ComplexNumber::abs(){
+	return sqrt(real*real + imag*imag);
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag,real)*180 / M_PI;
+}
+
+
+
+	
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
